4c028a298744628343c06c3606ce4e6cf695e34a
Imcccc <zqasder@gmail.com>
2015-02-09 21:31:14 -0500

    add new package

---
+++ Project/src/app/control/ActivityControler.java
@@ -0,0 +1,10 @@
+package app.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ActivityControler {
+
+}
