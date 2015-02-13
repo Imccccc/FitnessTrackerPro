0d87b3e32716777009e4ba7b12d29b1df41f8c88
HanchenLi <li1191@purdue.edu>
2015-02-12 19:41:07 -0500

    sql

--- Project/src/DBconnector/DBconnector.java
+++ Project/src/DBconnector/DBconnector.java
@@ -16,21 +16,28 @@ public abstract class DBconnector {
 	
 	public static void main(String args[]){
 		connect();
-		login("aaa","bbb");
 		login("lhc","123");
-		login("lhc","1234");
+		//createActivity("qwe", 0);
+		//createActivity("asd", 1);
+		reportToday(5.8);
 	}
 	
-	public static void connect(){
-		if (con!=null) return;
+	public static boolean isLoggedIn(){
+		return username!=null;
+	}
+	
+	public static int connect(){//0 success -1fail
+		if (con!=null) return -1;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(DBaddress+DBdatabase,DBuser,DBpass);
+			return 0;
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return -1;
 	}
 	
 	public static void close(){
@@ -42,7 +49,7 @@ public abstract class DBconnector {
 		con=null;
 	}
 	
-	public static int  login(String username, String password){//0 success -1fail
+	public static int login(String username, String password){//0 success -1fail
 		try{
 			Statement stmt = con.createStatement();
 			ResultSet result = stmt.executeQuery("SELECT * FROM account WHERE username='"+username+"' AND password='"+password+"'");
@@ -60,5 +67,73 @@ public abstract class DBconnector {
 			return -1;
 		}
 	}
+	
+	public static int createUser(String username, String password){//0 success -1fail
+		try{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("INSERT INTO account (username,password) VALUES ('"+username+"','"+password+"')");
+			System.out.println("Successfully creating account");
+			return 0;
+		}catch(SQLException e){
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public static int createActivity(String name, int unitsecond){// +num=id -1fail
+		try{
+			Statement stmt = con.createStatement();
+			ResultSet result = stmt.executeQuery("SELECT * FROM activity WHERE name='"+name+"'");
+			if (result.next()==true) {
+				System.out.println("Activity exists");
+				return -1;
+			}
+			stmt.executeUpdate("INSERT INTO activity (name,unitsecond) VALUES ('"+name+"','"+unitsecond+"')");
+			result = stmt.executeQuery("SELECT * FROM activity WHERE name='"+name+"'");
+			result.next();
+			int ret=result.getInt("activityid");
+			System.out.println("Successfully creating activity at ID"+ret);
+			return ret;
+		}catch(SQLException e){
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public static int addRating(int planid, double rating, String comments){//0seccess  -1fail
+		try{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("INSERT INTO rating (username,planid,rating,comments) VALUES ('"+username+"','"+planid+"','"+rating+"','"+comments+"')");
+			System.out.println("Successfully adding rating");
+			return 0;
+		}catch(SQLException e){
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public static int reportToday(double calories){//0seccess  -1fail
+		try{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("INSERT INTO personalexerciseamount (username,date,calories) VALUES ('"+username+"',curdate(),"+calories+")");
+			System.out.println("Successfully reporting data of today");
+			return 0;
+		}catch(SQLException e){
+			if (e.getErrorCode()==1062){
+				try {
+					Statement stmt = con.createStatement();
+					String a="UPDATE personalexerciseamount calories="+calories+" WHERE date=curdate() AND username='"+username+"'";
+					System.out.println(a);
+					stmt.executeUpdate("UPDATE personalexerciseamount SET calories="+calories+" WHERE date=curdate() AND username='"+username+"'");
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+					return -1;
+				}
+			}
+			return 0;
+		}
+	}
+	
+	
 }
 
