ec51a9cf6f283b095f37802aa91130481c6e89dd
HanchenLi <lihanchenhz@gmail.com>
2015-03-25 18:51:17 -0400

    planID getter setter

--- Project/src/app/model/dayAmount.java
+++ Project/src/app/model/dayAmount.java
@@ -3,4 +3,16 @@ package app.model;
 public class dayAmount {
 	public java.sql.Date date;
 	public double amount;
+	public java.sql.Date getDate() {
+		return date;
+	}
+	public void setDate(java.sql.Date date) {
+		this.date = date;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 }
